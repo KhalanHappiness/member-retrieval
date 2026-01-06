f2d6f3363009c0cd1f768898662bb68180b3b485
happiness <happinesskhalan@gmail.com>
2026-01-06 22:29:53 +0300

    updated the cors policy

--- Backend/app.py
+++ Backend/app.py
@@ -29,7 +29,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=24)
 CORS(app, 
      resources={
          r"/*": {
-             "origins": ["http://localhost:5173", "http://127.0.0.1:5173", "https://member-retrieval-zgdp.vercel.app/"],
+             "origins": ["http://localhost:5173", "http://127.0.0.1:5173", "https://member-retrieval-zgdp.vercel.app"],
              "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization"],
              "expose_headers": ["Set-Cookie"],
