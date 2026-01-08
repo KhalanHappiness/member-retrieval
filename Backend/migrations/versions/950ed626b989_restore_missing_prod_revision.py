499f18ac9416f4988db1901784c8e4edd3c7cbd2
happiness <happinesskhalan@gmail.com>
2026-01-08 23:16:21 +0300

    restore missing migration

---
+++ Backend/migrations/versions/950ed626b989_restore_missing_prod_revision.py
@@ -0,0 +1,24 @@
+"""restore missing prod revision
+
+Revision ID: 950ed626b989
+Revises: da383495ca70
+Create Date: 2026-01-08 23:10:35.650054
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '950ed626b989'
+down_revision = 'da383495ca70'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
